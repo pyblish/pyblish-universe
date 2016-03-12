b7c39939cec97a8c11aba4fa29c8f26bf0161298
Marcus Ottosson <marcus@abstractfactory.io>
2016-03-12 11:47:02 +0000

    Refine look

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 /*global Firebase*/
+/*global Handlebars*/
+
+function basename (path) {
+    return path.split(/[\\/]/).pop();
+}
 
 (function () {
     var ref = new Firebase("https://pyblish-web.firebaseio.com/events");
 
     ref.on("child_added", function(snapshot) {
         var item = snapshot.val();
-        append(item.name);
+        
+        item.authorUrl = item.author;
+        item.author = basename(item.author);
+    
+        item.targetUrl = item.target;
+        item.target = basename(item.target);
+
+        console.log(item, "added");
+        append(item);
     });
     
-    function append(name) {
-        $("#myEvents").append("<li><a href='url-here'>" + name + "</a></li>");
+    function append(item) {
+        // Template
+        var source   = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+
+        var context = item;
+        var html = template(context);
+    
+        $("#events").append(html);
     };
+    
 })();
