e4d4439edbf4f5590debf1717a3539ef0874a887
Marcus Ottosson <marcus@abstractfactory.io>
2016-03-12 17:10:46 +0000

    Added loader and alternaive template for larger events

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 /*global Firebase*/
 /*global Handlebars*/
 
-function basename (path) {
-    return path.split(/[\\/]/).pop();
-}
 
 (function () {
     var ref = new Firebase("https://pyblish-web.firebaseio.com/events");
+    
+    var templates = {
+        "small": Handlebars.compile($("#small-event-template").html()),
+        "large": Handlebars.compile($("#large-event-template").html()),
+    }
 
     ref.on("child_added", function(snapshot) {
+        $(".loader").hide();  // Loader visible by default
+
         var item = snapshot.val();
 
         item.authorName = basename(item.author);
-        item.targetName = basename(item.target);
+        item.targetName = basename(item.target, -2);
+        item.actionName = basename(item.action);
 
         console.log(item, "added");
         append(item);
     });
     
     function append(item) {
-        // Template
-        var source   = $("#entry-template").html();
-        var template = Handlebars.compile(source);
-
-        var context = item;
-        var html = template(context);
-    
+        var html = "body" in item ? templates.large(item) : templates.small(item);
         $("#events").append(html);
     };
     
 })();
+
+/**
+ * Return basename of path
+ * @param {string} path - Absolute path
+ * @param {string} slice - Optional levels at end to return; defaults to -1
+ */
+function basename (path, slice) {
+    return (path ? path : "").split(/[\\/]/).slice(slice ? slice : -1).join("/");
+}
